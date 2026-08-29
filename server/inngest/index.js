396e99d40f4b5c69e4fe2de824ecfeefa13fef83
TA-ashrafi <tahseenashrafi29@gmail.com>
2026-08-29 16:59:49 +0530

    feat: Add parallel email sending for attendance reminders

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -194,6 +194,7 @@ const attendanceReminderCron = inngest.createFunction(
             });
         }
 
+        await Promise.all(emailPromises)
         return {
             totalActive: activeEmployees.length,
             onLeave: onLeaveIds.length,
