1e7682fb199b70715d5306f03fc4dadb52e16b7e
TA-ashrafi <tahseenashrafi29@gmail.com>
2026-08-29 17:03:17 +0530

    fix: Remove redundant email promise handling in attendance reminder function

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -194,7 +194,6 @@ const attendanceReminderCron = inngest.createFunction(
             });
         }
 
-        await Promise.all(emailPromises)
         return {
             totalActive: activeEmployees.length,
             onLeave: onLeaveIds.length,
